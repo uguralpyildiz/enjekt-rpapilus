c11a1d8e709cbeb923155959506ac3f96eca1157
Ugur Alp Yildiz <77773407+uguralpyildiz@users.noreply.github.com>
2025-06-22 19:59:52 +0300

    Add files via upload

--- assets/script.js
+++ assets/script.js
@@ -33,140 +33,14 @@ window.addEventListener("scroll", reveal);
 
 
 
-const text = document.querySelector('.typing-text');
-
-
-const words = [
-    "Sonunda Aradığın Ajansı Buldun"
-];
-
-
-setTyper(text, words);
-
-function setTyper(element, words) {
-
-    const LETTER_TYPE_DELAY = 60;
-    const WORD_STAY_DELAY = 5000;
-
-    const DIRECTION_FORWARDS = 0;
-    const DIRECTION_BACKWARDS = 1;
-
-    var direction = DIRECTION_FORWARDS;
-    var wordIndex = 0;
-    var letterIndex = 0;
-
-    var wordTypeInterval;
-
-    startTyping();
-
-    function startTyping() {
-        wordTypeInterval = setInterval(typeLetter, LETTER_TYPE_DELAY);
-    }
-
-    function typeLetter() {
-        const word = words[wordIndex];
-
-        if (direction == DIRECTION_FORWARDS) {
-            letterIndex++;
-
-            if (letterIndex == word.length) {
-                direction = DIRECTION_BACKWARDS;
-                clearInterval(wordTypeInterval);
-                setTimeout(startTyping, WORD_STAY_DELAY);
-            }
-
-        } else if (direction == DIRECTION_BACKWARDS) {
-            letterIndex--;
-
-            if (letterIndex == 0) {
-                nextWord();
-            }
-        }
-
-        const textToType = word.substring(0, letterIndex);
-
-        element.textContent = textToType;
-    }
-
-    function nextWord() {
-
-        letterIndex = 0;
-        direction = DIRECTION_FORWARDS;
-        wordIndex++;
-
-        if (wordIndex == words.length) {
-            wordIndex = 0;
-        }
-
-    }
-}
-
-const loading = document.querySelector(".overlay-loading")
-
-
-setTimeout(() => {
-    loading.style.top = "-150%";
-    setTimeout(() => {
-        loading.style.display = "none"; 
-    }, 6500);
-    
-}, 3000);
 
 
 
-const texts = ['Front-end Developer', 'UI & UX Developer', 'React Developer'];
-var count = 0;
-var index = 0;
-var decrement = 0;
-var currentText = '';
-var letter = '';
 
-function sleep(delay) {
-    return new Promise(resolve => setTimeout(resolve, delay));
-}
 
-const typeWrite = async () => {
-    if (count == texts.length) {
-        count = 0;
-    }
-    currentWord = texts[count];
-    currentLetter = currentWord.slice(0, ++index);
-    document.querySelector(".job-bar").textContent = currentLetter;
-    if (index == currentWord.length) {
-        await sleep(1500);
-        while (index > 0) {
-            currentLetter = currentWord.slice(0, --index);
-            document.querySelector(".job-bar").textContent = currentLetter;
-            await sleep(50);
-        }
-        count++;
-        index = 0;
-        await sleep(500);
-    }
-    setTimeout(typeWrite, Math.random() * 200 + 50);
-}
-typeWrite();
 
-const downloadCv = document.querySelector("#downloadCv");
-const downloadText = document.querySelector(".download-text");
-const spinnerAnim = document.querySelector(".spinner-anim");
 
-downloadCv.addEventListener("click", () => {
-    downloadText.style.top = "-100px";
-    downloadCv.style.pointerEvents = "none";
-    setTimeout(() => {
-        downloadCv.innerHTML = '<span class="spinner-anim"><svg class="spinner" viewBox="0 0 50 50"><circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle></svg></span>'
-    }, 250);
 
-    setTimeout(() => {
-        downloadCv.innerHTML = '<span class="download-text"><i class="fa-solid fa-check"></i>Teşekkürler</span>'
-    }, 2500);
-
-    setTimeout(() => {
-        downloadCv.innerHTML = '<span class="download-text">Kolay Teklif Talebi <img width="16" height="16" src="https://img.icons8.com/e3e5e9/android/124/up-right-arrow.png" alt="up-right-arrow"></span>'
-        downloadCv.style.pointerEvents = "auto";
-    }, 7000);
-})
 
 
 
